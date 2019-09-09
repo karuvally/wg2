6fc07a0e4dbf9370177c8daf70dab6edf5e96240
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-09-09 21:38:24 +0530

    added linking to libcurl

--- wg2.c
+++ wg2.c
@@ -1,9 +1,25 @@
 // import the serious stuff
 #include<stdio.h>
+#include<curl/curl.h>
 
 
 // the main function
 int main()
 {
+	CURL *curl;
+	CURLcode response;
+
+	curl = curl_easy_init();
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
+		response = curl_easy_perform(curl);
+
+		if(response != CURLE_OK)
+			fprintf(stderr, "curl_easy_perform() failed: %s\n",
+				curl_easy_strerror(response));
+
+		curl_easy_cleanup(curl);
+	}
+
 	return 0;
 }
