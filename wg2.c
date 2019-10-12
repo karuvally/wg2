6ad1b2918177541aeac7030053c891805bd44023
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-10-12 20:55:06 +0530

    random cleanups

--- wg2.c
+++ wg2.c
@@ -4,31 +4,42 @@
 #include<curl/curl.h>
 
 // piece together weather URL from multiple strings
-def get_weather_url()
+char *get_weather_url()
 {
+	// essential variables
+	char *buffer = 0;
+	int length;
+
     // open the file
     FILE *url_file = fopen("weather_url", "r");
     
     // exit if the file does not exist
     if(url_file == NULL) {
-        printf("cannot load weather_url, exiting...")
-        exit(1)
+        printf("cannot load weather_url, exiting...");
+        exit(1);
+	}
+
+	// get the length of the file
+	fseek(url_file, 0, SEEK_END);
+	length = ftell(url_file);
     
-    return weather_url;
 }
 
 
 // the main function
 int main()
 {
+	// read weather URL from file
+    char *weather_url = get_weather_url();
+
+	/*
     // initialize curl
 	curl_global_init(CURL_GLOBAL_ALL);
     CURL *curl_handle;
     CURLcode result;
     curl_handle = curl_easy_init();
+	*/
     
-    // read weather URL from file
-    weather_url = get_weather_url()
-    
+	// just quit :D
 	return 0;
 }
