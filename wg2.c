8bd9f1a6ff0bdd65c3c9009283e8edf250bfd15b
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-10-12 14:04:12 +0530

    added get_weather_url()

--- wg2.c
+++ wg2.c
@@ -1,25 +1,34 @@
 // import the serious stuff
 #include<stdio.h>
+#include<stdlib.h>
 #include<curl/curl.h>
+
+// piece together weather URL from multiple strings
+def get_weather_url()
+{
+    // open the file
+    FILE *url_file = fopen("weather_url", "r");
+    
+    // exit if the file does not exist
+    if(url_file == NULL) {
+        printf("cannot load weather_url, exiting...")
+        exit(1)
+    
+    return weather_url;
+}
 
 
 // the main function
 int main()
 {
-	CURL *curl;
-	CURLcode response;
-
-	curl = curl_easy_init();
-	if(curl) {
-		curl_easy_setopt(curl, CURLOPT_URL, "https://google.com");
-		response = curl_easy_perform(curl);
-
-		if(response != CURLE_OK)
-			fprintf(stderr, "curl_easy_perform() failed: %s\n",
-				curl_easy_strerror(response));
-
-		curl_easy_cleanup(curl);
-	}
-
+    // initialize curl
+	curl_global_init(CURL_GLOBAL_ALL);
+    CURL *curl_handle;
+    CURLcode result;
+    curl_handle = curl_easy_init();
+    
+    // read weather URL from file
+    weather_url = get_weather_url()
+    
 	return 0;
 }
