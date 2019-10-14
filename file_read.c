805a4fd35a3d56e47535a9344bf853f3420cc291
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-10-14 21:42:02 +0530

    added code to get length of file

--- file_read.c
+++ file_read.c
@@ -4,22 +4,29 @@
 
 
 // read data from file 
-void read_from_file(char input_file[])
+void read_from_file(char file_path[])
 {
 	// essential variables
 	FILE *file_pointer;
+	long file_length;
 	char buffer[100];
 
 	// open the file
-	file_pointer = fopen(input_file, "r");
+	file_pointer = fopen(file_path, "r");
 
 	//exit if file does not exist
 	if(file_pointer == NULL) {
 		exit(1);
 	}
 
+	// get the file length
+	fseek(file_pointer, 0, SEEK_END);
+	file_length = ftell(file_pointer);
+	fseek(file_pointer, 0, SEEK_SET);
+
 	// read from file
 	fread(buffer, sizeof(char), 50, file_pointer);
+	buffer[50] = '\0';
 
 	// debug
 	printf("%s\n", buffer);
