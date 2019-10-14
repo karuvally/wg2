959e0ef229b0166cc7930313ccd8745f187de9bf
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-10-14 21:27:45 +0530

    file name can now be passed to read_from_file()

--- file_read.c
+++ file_read.c
@@ -4,14 +4,14 @@
 
 
 // read data from file 
-void read_from_file()
+void read_from_file(char input_file[])
 {
 	// essential variables
 	FILE *file_pointer;
 	char buffer[100];
 
 	// open the file
-	file_pointer = fopen("weather_url", "r");
+	file_pointer = fopen(input_file, "r");
 
 	//exit if file does not exist
 	if(file_pointer == NULL) {
@@ -20,12 +20,15 @@ void read_from_file()
 
 	// read from file
 	fread(buffer, sizeof(char), 50, file_pointer);
+
+	// debug
+	printf("%s\n", buffer);
 }
 
 
 // the main function
 int main()
 {
-	read_from_file();
+	read_from_file("weather_url");
 	return 0;
 }
