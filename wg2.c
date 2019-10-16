4e1f65eec64ab5f31658356c2016de4561116d94
Aswin Babu Karuvally <aswinbabuk@gmail.com>
2019-10-16 20:43:31 +0530

    added header

--- wg2.c
+++ wg2.c
@@ -1,48 +1,16 @@
+// Weather-Get2, rewrite of weather-get written for speed
+// Copyright 2019, Aswin Babu Karuvally
+
 // import the serious stuff
 #include<stdio.h>
 #include<stdlib.h>
 #include<curl/curl.h>
-
-// piece together weather URL from multiple strings
-char *get_weather_url()
-{
-	// essential variables
-	char *weather_url = 0;
-	int length;
-
-    // open the file
-    FILE *url_file = fopen("weather_url", "r");
-    
-    // exit if the file does not exist
-    if(url_file == NULL) {
-        printf("FATAL: Cannot load weather_url, exiting...\n");
-        exit(1);
-	}
-
-	// allocate memory to load the file 
-	fseek(url_file, 0, SEEK_END);
-	length = ftell(url_file);
-	weather_url = malloc(length);
-
-	// exit if memory cannot be allocated 
-	if(weather_url == NULL) {
-		printf("FATAL: No memory left, exiting...\n");
-		exit(1);
-	}
-
-	// read the file and close it
-	fread(weather_url, length, 1, url_file);
-	fclose(url_file);
-
-	return weather_url;
-}
 
 
 // the main function
 int main()
 {
 	// read weather URL from file
-    char *weather_url = get_weather_url();
 
 	/*
     // initialize curl
